08a03756df4a3dcdea32e9be1c90323cf0e9a206
amin-amat <amnamat@yahoo.com>
2016-02-16 09:32:41 -0800

    Update

---
+++ Javascripts/removeduplicate-list2ndhighestnum.js
@@ -0,0 +1,21 @@
+<script>
+var points = [40, 100, 1, 5, 25, 10,100,4,12,50];
+document.getElementById("demo").innerHTML = points;
+
+function removeTwin(array) {
+for(var i = array.length; i--;){
+	if (array[i] == array[i+1]) array.splice(i, 1);
+}
+
+}
+
+function myFunction() {
+    points.sort(function(a, b){return a-b});
+console.log(points);
+    removeTwin(points);
+console.log(points);
+numFind = (points.length) - 2;
+
+    document.getElementById("demo").innerHTML = points[numFind];
+}
+</script>
