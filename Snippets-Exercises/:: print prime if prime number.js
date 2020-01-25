13fd280822d8a1b88b8371ffcf9247771dfbe6d0
amin-amat <amnamat@yahoo.com>
2020-01-24 16:24:21 -0800

    added new js files

---
+++ Snippets-Exercises/:: print prime if prime number.js
@@ -0,0 +1,35 @@
+// print prime if prime number
+// add sum of all of non-prime numbers
+
+
+var catchAll = 0;
+
+for(i=2; i< 21; i++) {
+
+	for(j=2; j < i; j++){
+		if ( (i % 1 == 0) && (i % j == 1) ) {
+			console.log('prime');
+		}
+	
+}
+catchAll += i;
+
+}
+
+console.log(catchAll);
+
+var catchAll = "";
+for(i=2;i<21;i++){
+
+	if(i % i === 0 && i % 1 === 0){
+	console.log('Prime: ' + i);
+	}else {
+	catchAll +=i;
+	}
+	
+console.log(catchAll);
+}
+
+
+
+
---
+++ Snippets-Exercises/stoplight.js
@@ -0,0 +1,27 @@
+var mainElement = document.getElementById("stoplight");
+var one = mainElement.childNodes[1];
+var two = mainElement.childNodes[3];
+var three = mainElement.childNodes[5];
+
+mainElement.onclick = function run() {
+    if(one.classList.contains('active')) {
+        classReplace(one);
+        classAssign(three);
+    }else if(three.classList.contains('active')) {
+        classReplace(three);
+        classAssign(two);
+    }else if(two.classList.contains('active')) {
+        classReplace(two);
+        classAssign(one);
+    }
+}
+  
+function classReplace(target) {
+  target.className = target.className.replace(/(?:^|\s)active(?!\S)/,'');
+  return target.className;
+}
+
+function classAssign(target){
+  target.className += " active";
+  return target.className;
+}
